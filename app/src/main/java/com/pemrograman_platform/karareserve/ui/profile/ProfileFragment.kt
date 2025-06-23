92dc304cd0fa9a6b3c42f1e70c2d5ceaf40345ac
jonathanbasuki <basukijonathan4@gmail.com>
2025-06-23 10:12:27 +0700

    feat: add secure logout using firebase

--- app/src/main/java/com/pemrograman_platform/karareserve/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/pemrograman_platform/karareserve/ui/profile/ProfileFragment.kt
@@ -1,13 +1,21 @@
 package com.pemrograman_platform.karareserve.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.pemrograman_platform.karareserve.R
+import com.pemrograman_platform.karareserve.databinding.FragmentProfileBinding
+import com.pemrograman_platform.karareserve.ui.auth.LoginActivity
 
 class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,7 +24,31 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val user = FirebaseAuth.getInstance().currentUser
+        binding.textEmail.text = user?.email ?: "Unknown"
+
+        binding.menuLogout.setOnClickListener {
+            binding.progressContainer.visibility = View.VISIBLE
+
+            FirebaseAuth.getInstance().signOut()
+            Toast.makeText(requireContext(), "You're logged out!", Toast.LENGTH_SHORT).show()
+
+            // Navigasi ke LoginActivity (atau activity lainnya)
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
